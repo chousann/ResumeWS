a9497f838b39c4562e481b47d0aa2bc6fe05190d
chousann <1769862201@qq.com>
2019-04-28 20:31:40 +0800

    init

---
+++ src/main/java/com/example/ResumeWS/ResumeWsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ResumeWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeWsApplication.class, args);
+	}
+
+}
